678c3acc0e6d060f4cc88b34ab43bd0284f8866e
Shruti Naik <111608883+shrutiiiiin@users.noreply.github.com>
2022-12-06 01:30:15 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# e-commerce
+# mini-project
---
+++ registration.php
@@ -0,0 +1,114 @@
+<!-- <?php      
+        include('config.php');  
+        $username = $_POST['user'];  
+        $password = $_POST['pass'];  
+          
+            //to prevent from mysqli injection  
+            $username = stripcslashes($username);  
+            $password = stripcslashes($password);  
+            $username = mysqli_real_escape_string($con, $username);  
+            $password = mysqli_real_escape_string($con, $password);  
+          
+            $sql = "select *from login where username = '$username' and password = '$password'";  
+            $result = mysqli_query($con, $sql);  
+            $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+            $count = mysqli_num_rows($result);  
+              
+            if($count == 1){  
+                echo "<h1><center> Login successful </center></h1>";  
+            }  
+            else{  
+                echo "<h1> Login failed. Invalid username or password.</h1>";  
+            }     
+    ?>   -->
+<?php
+require_once "config.php";
+
+$username = $password = $confirm_password = "";
+$username_err = $password_err = $confirm_password_err = "";
+
+if ($_SERVER['REQUEST_METHOD'] == "POST"){
+
+    // Check if username is empty
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Username cannot be blank";
+    }
+    else{
+        $sql = "SELECT id FROM users WHERE username = ?";
+        $stmt = mysqli_prepare($conn, $sql);
+        if($stmt)
+        {
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+
+            // Set the value of param username
+            $param_username = trim($_POST['username']);
+
+            // Try to execute this statement
+            if(mysqli_stmt_execute($stmt)){
+                mysqli_stmt_store_result($stmt);
+                if(mysqli_stmt_num_rows($stmt) == 1)
+                {
+                    $username_err = "This username is already taken"; 
+                }
+                else{
+                    $username = trim($_POST['username']);
+                }
+            }
+            else{
+                echo "Something went wrong";
+            }
+        }
+    }
+
+    mysqli_stmt_close($stmt);
+
+
+// Check for password
+if(empty(trim($_POST['password']))){
+    $password_err = "Password cannot be blank";
+}
+elseif(strlen(trim($_POST['password'])) < 5){
+    $password_err = "Password cannot be less than 5 characters";
+}
+else{
+    $password = trim($_POST['password']);
+}
+
+// Check for confirm password field
+if(trim($_POST['password']) !=  trim($_POST['confirm_password'])){
+    $password_err = "Passwords should match";
+}
+
+
+// If there were no errors, go ahead and insert into the database
+if(empty($username_err) && empty($password_err) && empty($confirm_password_err))
+{
+    $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+    $stmt = mysqli_prepare($conn, $sql);
+    if ($stmt)
+    {
+        mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+
+        // Set these parameters
+        $param_username = $username;
+        $param_password = password_hash($password, PASSWORD_DEFAULT);
+
+        // Try to execute the query
+        if (mysqli_stmt_execute($stmt))
+        {
+            header("location: login.php");
+        }
+        else{
+            echo "Something went wrong... cannot redirect!";
+        }
+    }
+    mysqli_stmt_close($stmt);
+}
+mysqli_close($conn);
+}
+
+?>
+
+
+
+
